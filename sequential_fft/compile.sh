8b8f06094e2edee00d1dfd7455d20ee5c8fa7b80
Ronghang Hu <ronghang.hu@gmail.com>
2019-04-16 17:21:20 -0700

    fix compiling for newer TF version
    c.f. https://github.com/tensorflow/tensorflow/issues/13607

--- sequential_fft/compile.sh
+++ sequential_fft/compile.sh
@@ -1,12 +1,17 @@
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 nvcc -std=c++11 -c -o sequential_batch_fft_kernel.cu.o \
   sequential_batch_fft_kernel.cu.cc \
+  -D_GLIBCXX_USE_CXX11_ABI=0 -DNDEBUG \
+  -L$TF_LIB -ltensorflow_framework \
   -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 g++ -std=c++11 -shared -o ./build/sequential_batch_fft.so \
   sequential_batch_fft_kernel.cu.o \
   sequential_batch_fft.cc \
+  -D_GLIBCXX_USE_CXX11_ABI=0 -DNDEBUG \
+  -L$TF_LIB -ltensorflow_framework \
   -I $TF_INC -fPIC \
   -lcudart -lcufft -L/usr/local/cuda/lib64
 
